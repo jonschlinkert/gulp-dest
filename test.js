480fb90eec6debe3e77c3da2038bfb22c754bcee
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-22 18:28:30 -0500

    adds tests

--- test.js
+++ test.js
@@ -47,7 +47,28 @@ describe('dest()', function () {
     stream.end();
   });
 
-  it('should set the destination extension:', function (cb) {
+  it('should use options.base:', function (cb) {
+    var stream = dest({base: 'foo/'});
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.md'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'foo/post.md');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should set the destination extension with `ext`:', function (cb) {
     var stream = dest({ext: '.html'});
     var buffer = [];
 
@@ -63,6 +84,90 @@ describe('dest()', function () {
     stream.on('end', function () {
       assert.equal(buffer.length, 1);
       assert.equal(buffer[0].relative, 'post.html');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should set the destination extension with `extname`:', function (cb) {
+    var stream = dest({extname: '.html'});
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.md'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'post.html');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should use the properties defined in the options:', function (cb) {
+    var stream = dest('dist', {basename: 'foo', ext: '.html'});
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.md'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'dist/foo.html');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should work with double extensions:', function (cb) {
+    var stream = dest('dist/');
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.min.js'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'dist/post.min.js');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should work with double extensions:', function (cb) {
+    var stream = dest('dist', {basename: 'foo', ext: '.min.js'});
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.md'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'dist/foo.min.js');
       cb();
     });
     stream.end();
@@ -84,6 +189,73 @@ describe('dest()', function () {
     stream.on('end', function () {
       assert.equal(buffer.length, 1);
       assert.equal(buffer[0].relative, 'dist/post.html');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should substitute `:props` with file path parts:', function (cb) {
+    var stream = dest('dist/:name.foo');
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.md'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'dist/post.foo');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should substitute `:props` with options props:', function (cb) {
+    var stream = dest(':dir/:name.:a:b:c', {dir: 'dist', a: 'A', b: 'B', c: 'C'});
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.md'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'dist/post.ABC');
+      cb();
+    });
+    stream.end();
+  });
+
+  it('should allow a function to be used:', function (cb) {
+    var stream = dest('dist/:name', function (file, parsed) {
+      file.name = 'foo.bar';
+      return file;
+    });
+
+    var buffer = [];
+
+    stream.write(new File({
+      base: __dirname,
+      path: __dirname + '/post.md'
+    }));
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      assert.equal(buffer.length, 1);
+      assert.equal(buffer[0].relative, 'dist/foo.bar');
       cb();
     });
     stream.end();
