87ae63a508c4a10e51df2e6a3a8128ba82d15332
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-22 18:28:23 -0500

    run update

--- index.js
+++ index.js
@@ -4,39 +4,39 @@ var path = require('path');
 var gutil = require('gulp-util');
 var through = require('through2');
 var extend = require('extend-shallow');
-var parsePath = require('parse-filepath');
 var rewrite = require('rewrite-ext');
+var parse = require('parse-filepath');
+var rte = require('rte');
 
+module.exports = function destPath(dest, fn, options) {
+  if (typeof fn !== 'function') {
+    options = fn;
+    fn = null;
+  }
 
-module.exports = function destPath(dest, options) {
   if (typeof dest !== 'string') {
     options = dest;
     dest = '.';
   }
 
-  return through.obj(function (file, encoding, cb) {
+  return through.obj(function (file, enc, cb) {
     var opts = extend({}, options);
+    var parsed = extend(parse(file.path), opts);
+    if (typeof fn === 'function') {
+      parsed = fn(parsed, file);
+    }
 
     try {
-      // get the new dest extension based on the engine extension
-      var ext = opts.ext || path.extname(file.path);
+      if (dest.indexOf(':') !== -1) {
+        file.path = rte(dest, parsed);
+      } else {
+        // get the new dest extension based on the engine extension
+        var ext = opts.ext || opts.extname || path.extname(file.path);
+        var name = opts.basename || file.relative;
+        var base = opts.base || opts.dirname || file.base;
 
-      // calculate the new destination path
-      file.path = rewrite(path.join(file.base, dest, file.relative), ext);
-
-      // parse the path and update `file.data.dest`
-      // with the new properties
-      if (file.data) {
-        var parsed = parsePath(file.path);
-        file.data.dest = file.data.dest || {};
-        for (var key in parsed) {
-          file.data.dest[key] = parsed[key];
-        }
-        file.data.dest = extend(file.data.dest, {
-          path: file.path,
-          ext: parsed.extname
-        });
-        file.data.dest.path = dest;
+        // calculate the new destination path
+        file.path = rewrite(path.join(base, dest, name), ext);
       }
 
       this.push(file);
