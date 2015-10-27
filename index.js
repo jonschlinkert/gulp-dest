19374a97b433a583f2b6d599374d9dc248c4b229
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-27 05:02:48 -0400

    use lazy-caching

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 'use strict';
 
 var path = require('path');
-var gutil = require('gulp-util');
-var through = require('through2');
-var extend = require('extend-shallow');
-var rewrite = require('rewrite-ext');
-var parse = require('parse-filepath');
-var rte = require('rte');
+var utils = require('./utils');
 
 module.exports = function destPath(dest, fn, options) {
   if (typeof fn !== 'function') {
@@ -19,16 +14,17 @@ module.exports = function destPath(dest, fn, options) {
     dest = '.';
   }
 
-  return through.obj(function (file, enc, cb) {
-    var opts = extend({}, options);
-    var parsed = extend(parse(file.path), opts);
+  return utils.through.obj(function (file, enc, cb) {
+    var opts = utils.extend({}, options);
+    var parsed = utils.extend(utils.parse(file.path), opts);
     if (typeof fn === 'function') {
       parsed = fn(parsed, file);
     }
 
     try {
       if (dest.indexOf(':') !== -1) {
-        file.path = rte(dest, parsed);
+        var interpolate = utils.placeholders(opts);
+        file.path = interpolate(dest, parsed);
       } else {
         // get the new dest extension based on the engine extension
         var ext = opts.ext || opts.extname || path.extname(file.path);
@@ -36,12 +32,12 @@ module.exports = function destPath(dest, fn, options) {
         var base = opts.base || opts.dirname || file.base;
 
         // calculate the new destination path
-        file.path = rewrite(path.join(base, dest, name), ext);
+        file.path = utils.rewrite(path.join(base, dest, name), ext);
       }
 
       this.push(file);
     } catch (err) {
-      this.emit('error', new gutil.PluginError('gulp-dest', err));
+      this.emit('error', new utils.gutil.PluginError('gulp-dest', err));
     }
 
     return cb();
